1db45eade69335759249acd2a33069797f909e6c
Jude kuti <judekuti@gmail.com>
2020-07-05 18:51:58 -0500

    use dotenv and populate from env

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,2 +1,21 @@
 import * as firebase from 'firebase';
 import firestore from 'firebase/firestore';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const settings = { timestampsInSnapshots: true }
+
+const config = {
+    apiKey: process.env.apiKey,
+    authDomain: process.env.authDomain,
+    databaseURL: process.env.databaseURL,
+    projectId: process.env.projectId,
+    storageBucket: process.env.storageBucket,
+    messagingSenderId: process.env.messagingSenderId
+};
+firebase.initializeApp(config);
+
+firebase.firestore().settings(settings);
+
+export default firebase;
