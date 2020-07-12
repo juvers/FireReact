c0692e0562256a89fc191ac9224967604b7d676f
Jude kuti <judekuti@gmail.com>
2020-07-12 01:32:29 -0500

    Import Link from react-router-dom for anchoring

--- src/App.js
+++ src/App.js
@@ -1,13 +1,75 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Link } from 'react-router-dom';
 import './App.css';
+import firebase from './Firebase';
 
-function App() {
-    return ( 
-    <div className = "App" >
-        FireBase React 
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.ref = firebase.firestore().collection('boards');
+    this.unsubscribe = null;
+    this.state = {
+      boards: []
+    }
+  }
+
+  onCollectionUpdate = (querySnapshot) => {
+    const boards = [];
+    querySnapshot.forEach(doc => {
+      const {title, description, author} = doc.data();
+      boards.push({
+        key: doc.id,
+        doc,
+        title,
+        description,
+        author
+      });
+    })
+    this.setState({boards})
+  }
+
+  componentDidMount() {
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
+  }
+
+  render(){
+    return (
+      <div className="container">
+        <div className="panel panel-default">
+          <div className="panel-heading">
+            <h3 className="panel-title">
+            Board List
+            </h3>
+          </div>
+          <div className="panel-body">
+            <h4>
+              <Link to="/create">Add Board</Link>
+            </h4>
+            <table className="table table-stripe">
+              <thead>
+                <tr>
+                  <th>Title</th>
+                  <th>Description</th>
+                  <th>Author</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.boards.map(board =>
+                  <tr>
+                    <td>
+                      <Link to={`'show/$boadr.key`}>{board.title}</Link>
+                    </td>
+                    <td>{board.description}</td>
+                    <td>{board.author}</td>
+                  </tr> )}
+              </tbody>
+            </table>
+
+          </div>
         </div>
-    );
+      </div>
+    )
+  }
 }
 
 export default App;
--- src/Firebase.js
+++ src/Firebase.js
@@ -1,18 +1,19 @@
 import * as firebase from 'firebase';
 import firestore from 'firebase/firestore';
-import dotenv from 'dotenv';
+// import dotenv from 'dotenv';
 
-dotenv.config();
+// dotenv.config();
 
 const settings = { timestampsInSnapshots: true }
 
 const config = {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    databaseURL: process.env.databaseURL,
-    projectId: process.env.projectId,
-    storageBucket: process.env.storageBucket,
-    messagingSenderId: process.env.messagingSenderId
+
+    apiKey: "AIzaSyAJFh5H9XOb_eh4XgmX4B_t2Pe5J1ZbQkc",
+    authDomain: "react-base-82c7b.firebaseapp.com",
+    databaseURL: "https://react-base-82c7b.firebaseio.com",
+    projectId: "react-base-82c7b",
+    storageBucket: "react-base-82c7b.appspot.com",
+    messagingSenderId: "380910866350"
 };
 firebase.initializeApp(config);
 
