2b69d538e5cd3d751b70244c67f6ff5d4259bd2f
Jude kuti <judekuti@gmail.com>
2020-07-20 00:48:55 -0500

    Complete CRUD

--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import './App.css';
 import firebase from './Firebase';
 
-class App extends React.Component {
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.ref = firebase.firestore().collection('boards');
     this.unsubscribe = null;
     this.state = {
       boards: []
-    }
+    };
   }
 
   onCollectionUpdate = (querySnapshot) => {
     const boards = [];
-    querySnapshot.forEach(doc => {
-      const {title, description, author} = doc.data();
+    querySnapshot.forEach((doc) => {
+      const { title, description, author } = doc.data();
       boards.push({
         key: doc.id,
-        doc,
+        doc, // DocumentSnapshot
         title,
         description,
-        author
+        author,
       });
-    })
-    this.setState({boards})
+    });
+    this.setState({
+      boards
+   });
   }
 
   componentDidMount() {
-    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate)
+    this.unsubscribe = this.ref.onSnapshot(this.onCollectionUpdate);
   }
 
-  render(){
+  render() {
     return (
-      <div className="container">
-        <div className="panel panel-default">
-          <div className="panel-heading">
-            <h3 className="panel-title">
-            Board List
+      <div class="container">
+        <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">
+              BOARD LIST
             </h3>
           </div>
-          <div className="panel-body">
-            <h4>
-              <Link to="/create">Add Board</Link>
-            </h4>
-            <table className="table table-stripe">
+          <div class="panel-body">
+            <h4><Link to="/create">Add Board</Link></h4>
+            <table class="table table-stripe">
               <thead>
                 <tr>
                   <th>Title</th>
@@ -56,19 +56,17 @@ class App extends React.Component {
               <tbody>
                 {this.state.boards.map(board =>
                   <tr>
-                    <td>
-                      <Link to={`'show/$boadr.key`}>{board.title}</Link>
-                    </td>
+                    <td><Link to={`/show/${board.key}`}>{board.title}</Link></td>
                     <td>{board.description}</td>
                     <td>{board.author}</td>
-                  </tr> )}
+                  </tr>
+                )}
               </tbody>
             </table>
-
           </div>
         </div>
       </div>
-    )
+    );
   }
 }
 
